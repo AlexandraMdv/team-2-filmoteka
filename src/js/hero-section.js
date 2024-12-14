b9621f31243c4e8825c119e1ee2ab229fac67c5b
minodoragradinariu <minodoragradinariu@yahoo.ro>
2024-12-14 18:03:31 +0200

    v1

---
+++ src/js/hero-section.js
@@ -0,0 +1,22 @@
+// Hero Section
+
+function navigateHome() {
+  window.location.href = 'index.html';
+}
+
+const darkModeToggle = document.getElementById('darkModeToggle');
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+  document.body.classList.add('dark-mode');
+  darkModeToggle.checked = true;
+}
+
+darkModeToggle.addEventListener('change', () => {
+  if (darkModeToggle.checked) {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled'); // Salvează preferința
+  } else {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'disabled');
+  }
+});
