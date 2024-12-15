f90d324059fcc28da34953314c55e7237bbd8e7b
Erdelyi Gyongyi <erdelyi_gyongyi@yahoo.com>
2024-12-15 22:53:49 +0200

    functioneaza HTML, CSS

--- src/js/header.js
+++ src/js/header.js
@@ -1,3 +1,5 @@
+const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1%27'
+const API_KEY = '918aba14d75940fa7e0d80373b9ee894';
 const homeButton = document.querySelector('.home');
 const searchInput = document.querySelector('.search-box input');
 
