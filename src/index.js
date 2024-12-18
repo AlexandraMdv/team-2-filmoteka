91a0ea707844ab29a38e31b7e2ddd3f9db046b6f
Dragostin99 <dragostin_lcs@yahoo.com>
2024-12-18 20:47:15 +0200

    test4

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { refs } from './js/refs';
 import './js/api.js';
 import './js/cards-fetch.js';
 import './js/cards.js';
 import './js/carousel.js';
+import './js/about-modal.js';
+import './js/modalCards.js';
